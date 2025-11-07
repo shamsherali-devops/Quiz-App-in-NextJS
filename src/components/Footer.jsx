72527f0ea28d81297c1848081f4c7ec692d128ed
shamsherali-devops <shamsheralidev@gmail.com>
2025-11-07 08:03:31 +0500

    quiz app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => (
+  <footer className="bg-gray-800 text-white py-6 px-4 text-center">
+    <p>
+      &copy; 2025-26{' '}
+      <a
+        href="https://aman-kumar-sinha.vercel.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-blue-400 hover:underline"
+      >
+        Shamsher Ali
+      </a>. All rights reserved.
+    </p>
+  </footer>
+);
+
+export default Footer;
